65bc07140c8f803593b4e259d1963acf6fe23302
SpeedyCraftah <homodude55@gmail.com>
2020-12-30 21:08:20 +0000

    Add project files.

---
+++ discord input rpc tracker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discord_input_rpc_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
